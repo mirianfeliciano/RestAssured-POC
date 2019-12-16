20a2eea1b37a1810a5cfdc96314dba7adea49b5b
guina.costa <agnaldo.almeida@iteris.com.br>
2019-12-16 16:42:22 -0300

    configuracoes

--- src/test/java/utils/ConstantsHelper.java
+++ src/test/java/utils/ConstantsHelper.java
@@ -3,11 +3,11 @@ package utils;
 public interface ConstantsHelper {
 
     //URL BASE
-    public static final String BASE_URL = "http://localhost";
+    public static final String BASE_URL = "http://algamoneyapi-env.98hpppxyzr.us-east-1.elasticbeanstalk.com";
 
     public static final String BASE_URL_LOGIN = "/oauth/token";
 
     public static final String BASE_URL_PESSOAS = "/pessoas";
 
-    public static final int PORT = 8080;
+    public static final int PORT = 80;
 }
